d059cb8d5b01ea57aca5fd206577c2611ef0f84b
ponzs212 <gavinsatria885@gmail.com>
2026-04-10 21:43:35 +0700

    Update and rename index.js to number.json

--- index.js
+++
@@ -1,9 +0,0 @@
-import fetch from "node-fetch";
-import TelegramBot from "node-telegram-bot-api";
-
-const TOKEN = process.env.BOT_TOKEN;
-const bot = new TelegramBot(TOKEN, { polling: true });
-
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "👋 Halo! Bot ini versi terbaru dari GitHub.");
-});
